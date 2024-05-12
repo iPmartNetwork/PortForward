6ddbf3492903f3e71e8bd921de2dc537eb4df0b5
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-12 19:31:47 +0330

    Create PortForward.sh

---
+++ PortForward.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+echo
+echo "
+____________________________________________________________________________________
+        ____                             _     _                                     
+    ,   /    )                           /|   /                                  /   
+-------/____/---_--_----__---)__--_/_---/-| -/-----__--_/_-----------__---)__---/-__-
+  /   /        / /  ) /   ) /   ) /    /  | /    /___) /   | /| /  /   ) /   ) /(    
+_/___/________/_/__/_(___(_/_____(_ __/___|/____(___ _(_ __|/_|/__(___/_/_____/___\__
+                                                                                     
+"
+echo "***** https://github.com/ipmartnetwork *****"
+
+# User must run the script as root
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run this script as root"
+	exit 1
+fi
+
+distro=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+thisServerIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+networkInterfaceName=$(ip -o -4 route show to default | awk '{print $5}')
+
+if [[ $distro != "ubuntu" ]]; then
+	echo "distro not supported please use ubuntu"
+	exit 1
+fi
+
+echo "Select one of the following options"
+echo "   1) Server tunnel"
+echo "   2) Remove the tunnel"
+echo "   3) View the Forwarded IP"
+echo "   3) exit"
+
+read -r -p "Please select one [1-2-3-4]: " -e OPTION
+
+case $OPTION in
+1)
+	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+	sysctl -p
+	iptables -t nat -I PREROUTING -p tcp --dport 810 -j DNAT --to-destination "$thisServerIP"
+	iptables -t nat -I PREROUTING -p udp --dport 810 -j DNAT --to-destination "$thisServerIP"
+	iptables -t nat -I PREROUTING -p tcp --dport 4143 -j DNAT --to-destination "$thisServerIP"
+	iptables -t nat -I PREROUTING -p udp --dport 4143 -j DNAT --to-destination "$thisServerIP"
+	iptables -t nat -I PREROUTING -p tcp --dport 22 -j DNAT --to-destination "$thisServerIP"
+	echo "Enter foreign server IP:"
+	read -r foreignVPSIP
+	iptables -t nat -A PREROUTING -j DNAT --to-destination "$foreignVPSIP"
+	iptables -t nat -A POSTROUTING -j MASQUERADE -o "$networkInterfaceName"
+	echo "tunnel is done Wait for other steps to take"
+	apt update -y
+	apt upgrade -y
+	apt install iptables-persistent -y
+	sudo netfilter-persistent save
+	iptables-save > /etc/iptables/rules.v4
+	ip6tables-save > /etc/iptables/rules.v6
+	echo "Your tunnel finished"
+	;;
+
+2)
+	echo "Your forward port was removed"
+	sudo iptables -t nat -F
+	;;
+
+3)
+	iptables -t nat -L --line-numbers
+	;;
+
+4)
+	exit
+	;;
+
+esac
